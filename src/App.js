42fca46ebc5f5901365fa2426f7fa9f7730be5f1
LM10Saksham <sakshambarcelona@gmail.com>
2018-01-29 16:54:38 +0530

    Linking remains

--- src/App.js
+++ src/App.js
@@ -2,6 +2,61 @@ import React from 'react';
 import ChatBot from 'react-simple-chatbot';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {Helmet} from 'react-helmet';
+
+var express = require('express');
+var request = require('request');
+var cookieParser = require('cookie-parser');
+var bodyParser=require('body-parser');
+var path = require('path');
+var app = express();
+
+var watson = require("watson-developer-cloud");
+
+var conversation = new watson.ConversationV1({
+    username: '5e9aee44-052f-463e-9b9c-eb0c899ea045',
+    password: 'ZHUBaZJsaVfu',
+    version_date: '2017-05-26'
+});
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+var context=null;
+
+app.get('/', function (req, res) {
+  conversation.message({
+      input: {'text': req.body.text},
+      workspace_id: '1cf9cd65-5506-4bc2-a31d-06872dec90dd',
+      context: context
+  },  function(err, response) {
+      if (err)
+      console.log('error:', err);
+      else
+     console.log(JSON.stringify(response, null, 2));
+     context= response.context;
+     res.sendfile(path.join(__dirname, 'public', 'input.html'));
+    });
+});
+
+app.post('/send',(req, res) => {
+    conversation.message({
+      input: {'text': req.body.text},
+      workspace_id: '1cf9cd65-5506-4bc2-a31d-06872dec90dd',
+      context: context
+  },  function(err, response) {
+      if (err)
+      console.log('error:', err);
+      else
+     console.log(JSON.stringify(response, null, 2));
+     context= response.context;
+     res.send(response.output.text);
+    });
+});
+
+app.listen(3000, function () {
+    console.log('App listening on port 8080!');
+});
+
 // all available props
 
 
@@ -11,42 +66,69 @@ const backStyle = {
   
   width : "100%"
 }
-var request = new XMLHttpRequest();
-const steps = [
-   {
-  id: '1',
-  message: 'Hi. It looks like a nice drive today. What would you like me to do?',
-  trigger: '2',
-},
-{
-  id: '2',
-  message:  request.onreadystatechange=function({ previousValue, steps }){
-              if(request.readyState==4){
-                  if(request.status==200)
+
+function onClick(previousValue, steps){
+  var text = previousValue;
+          var request = new XMLHttpRequest();
+          request.onreadystatechange=function(){
+              if(request.readyState===4){
+                  if(request.status===200)
                       document.getElementById('resp').innerHTML='watson says:- '+request.responseText;
                   else
                       alert('something went wrong');
               }
               
-          },
-  trigger: ({ value, steps }) => '3',
+          };
+          request.open('POST','/send', true);
+          request.setRequestHeader('Content-Type','application/json');
+          request.send(JSON.stringify({text:text})); 
+      
+}
+
+const steps = [
+   {
+    id: 'first',
+    message: 'Hello World',
+    trigger: 'second',
 },
 {
-  id: '3',
-  message: 'Bye',
-  end: true,
+  id: 'second',
+  user: true,
+  trigger: 'third',
+},
+{
+  id: 'third',
+  message: function ({ previousValue, steps }){
+    var text = previousValue;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange=function(){
+              if(request.readyState===4){
+                  if(request.status===200){}
+                      var response = request.responseText;
+                  }
+                  else{
+                      alert('something went wrong');
+                }
+              }
+    request.open('POST','/send', true);
+          request.setRequestHeader('Content-Type','application/json');
+          request.send(JSON.stringify({text:text}));
+    return text
+  },
+  end:true,
 }
 ];
 
 const ThemedExample = () => (
   
   <MuiThemeProvider>
+  <script src = "./server.js"></script>
   <Helmet>
                 <style>{'body { background-color: black; }'}</style>
             </Helmet>
   <ChatBot steps={steps} style = {{marginTop : 75, width : '100%', height : "100%"}} inputStyle = {{height : '100%'}}/>;
-  
-  </MuiThemeProvider> 
+
+  </MuiThemeProvider>
 );
 
 export default ThemedExample;
